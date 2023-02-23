4b4288379aa634e10ff331e574d646fe5f4f9bac
Oussama Mrabet <oussemamrabet110@gmail.com>
2023-02-23 18:48:35 +0100

    added new feature on -zip_cracker-

--- zip cracker/zipcracker.py
+++ zip cracker/zipcracker.py
@@ -13,8 +13,9 @@ with open(passwordListFile,'rb') as text:
             with zipfile.ZipFile(lockedZipFile,'r') as zf:
                 zf.extractall(pwd=password)
                 data = zf.namelist()[0]
+                size = zf.getinfo(data).file_size
                 print('''********************\n[+] Password found! --> %s\n ~%s\n ~%s\n********************''' 
-                    % (password.decode('utf8'),data))
+                    % (password.decode('utf8'),data,size))
                 break
         except:
             number = count
